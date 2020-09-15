5df17745d3471e54c6624727ea6b75c303db26ea
José Nogueira <josej@bdnes.gov.br>
2020-09-15 18:46:27 -0300

    Several changes in the RBBRegistry

--- Back-Blockchain/contracts/RBBRegistry.sol
+++ Back-Blockchain/contracts/RBBRegistry.sol
@@ -3,38 +3,29 @@ pragma solidity ^0.5.0;
 import "@openzeppelin/contracts/ownership/Ownable.sol";
 import "./RBBLib.sol";
 
-/*
-Acho que LegalEntityInfo deveria conter id, ao invés de cnpj
-No caso de pessoa jurídica, o id é o cnpj
-No caso de pessoa física, um link para um cpf (por causa da LGPD).
-*/
 contract RBBRegistry is Ownable() {
 
-    /**
-        The account of clients and suppliers are assigned to states.
+    /** Account States:
         AVAILABLE - The account is not yet assigned any role.
         WAITING_VALIDATION - The account was linked to a legal entity but it still needs to be validated
         VALIDATED - The account was validated
-        INVALIDATED_BY_VALIDATOR - The account was invalidated
-        INVALIDATED_BY_CHANGE - The client or supplier changed the ethereum account so the original one must be invalidated.
+        INVALIDATED - The account was invalidated
+        PAUSED - The account was paused and can be reactived anytime (it returns to VALIDATED)
      */
-    enum BlockchainAccountState {AVAILABLE,WAITING_VALIDATION,VALIDATED,INVALIDATED_BY_VALIDATOR,INVALIDATED_BY_CHANGE}
-    BlockchainAccountState blockchainState; //Not used. Defined to create the enum type.
+    enum BlockchainAccountState {AVAILABLE,WAITING_VALIDATION,VALIDATED,INVALIDATED,PAUSED}
+    BlockchainAccountState blockchainState; /* Variable not used, only defined to create the enum type. */
 
-//TODO: deverá ser um conjunto de validadores, com possibilidade de incluir/remover novos,
-//TODO: owner n pode ser BNDES
-//Tratar como address ou como id?
-    address responsibleForRegistryValidation;
+    enum BlockchainAccountRole {NORMAL, ADMIN, SYSTEM}
+    BlockchainAccountRole blockchainRole;  /* Variable not used, only defined to create the enum type. */
 
-    //todo: avaliar criacao de papel de invalidador para casos ad-hoc
-
-    /**
-        Describes the Legal Entity - clients or suppliers
+     /**
+        This represents a real life organization
      */
     struct LegalEntityInfo {
         uint id; //Brazilian identification of legal entity
         string idProofHash; //hash of declaration
         BlockchainAccountState state;
+        BlockchainAccountRole role;
     }
 
     /**
@@ -43,113 +34,104 @@ contract RBBRegistry is Ownable() {
     mapping(address => LegalEntityInfo) public legalEntitiesInfo;
 
     /**
-        Links Legal Entity to Ethereum address.
+        Links Legal Entity IDs to Ethereum addresses.
      */
-    mapping(uint => address) legalEntityId_To_Addr;
-
-
-    /**
-        Links Ethereum addresses to the possibility to change the account
-        Since the Ethereum account can be changed once, it is not necessary to put the bool to false.
-        TODO: Discuss later what is the best solution to this
-     */
-    //mapping(address => bool) public legalEntitiesChangeAccount;
-
+    mapping(uint => address[]) legalEntityId_To_Addr;
 
     event AccountRegistration(address addr, uint id,  string idProofHash);
-    event AccountChange(address oldAddr, address newAddr, uint id, string idProofHash);
-    event AccountValidation(address addr, uint id);
-    event AccountInvalidation(address addr, uint id);
+    event AccountValidation(address addr, uint id, address responsible);
+    event AccountInvalidation(address addr, uint id, address responsible);
+    event AccountAdminUpgrade(address addr, uint id, address responsible);
+    event AccountPaused(address addr, uint id, address responsible);
+    event AccountUnpaused(address addr, uint id, address responsible);
 
-    constructor (uint idResposibleForValidation) public {
-        responsibleForRegistryValidation = msg.sender;
-        legalEntitiesInfo[msg.sender] = LegalEntityInfo(idResposibleForValidation, "", BlockchainAccountState.VALIDATED);
-        legalEntityId_To_Addr[idResposibleForValidation] = responsibleForRegistryValidation;
+    constructor (uint idResposibleForValidation) public {        
+        legalEntitiesInfo[msg.sender] = LegalEntityInfo(idResposibleForValidation, "", BlockchainAccountState.VALIDATED, BlockchainAccountRole.SYSTEM);
+        legalEntityId_To_Addr[idResposibleForValidation].push(msg.sender);
         emit AccountRegistration(msg.sender, idResposibleForValidation, "");
     }
 
    /**
-    * Link blockchain address with ID - It can be a cliente or a supplier
-    * The link still needs to be validated by BNDES
-    * This method can only be called by BNDESToken contract because BNDESToken can pause.
-    * @param cnpj Brazilian identifier to legal entities
+    * Link blockchain address with ID
+    * @param id Brazilian identifier to legal entities
     * @param idProofHash The legal entities have to send BNDES a PDF where it assumes as responsible for an Ethereum account.
     *                   This PDF is signed with eCNPJ and send to BNDES.
     */
-    function registryLegalEntity(uint cnpj, string memory idProofHash)
-        public {
+    function registryLegalEntity(uint id, string memory idProofHash) public {
         
         address addr = msg.sender;
 
-        // Endereço não pode ter sido cadastrado anteriormente
         require (isAvailableAccount(addr), "Endereço não pode ter sido cadastrado anteriormente");
 
-        require (RBBLib.isValidHash(idProofHash), "O hash da declaração é inválido");
+        if ( RBBLib.isValidHash(idProofHash) ) { 
+            legalEntitiesInfo[addr] = LegalEntityInfo(id, idProofHash, 
+                                                       BlockchainAccountState.WAITING_VALIDATION, 
+                                                       BlockchainAccountRole.ADMIN );
+        } else {
+            legalEntitiesInfo[addr] = LegalEntityInfo(id, idProofHash, 
+                                                       BlockchainAccountState.WAITING_VALIDATION, 
+                                                       BlockchainAccountRole.NORMAL );
+        }
 
-        address account = getBlockchainAccount(cnpj);
+        legalEntityId_To_Addr[id].push(addr);
 
-        require (isAvailableAccount(account), "CNPJ Já está associado. Use a função Troca.");
-
-        legalEntitiesInfo[addr] = LegalEntityInfo(cnpj, idProofHash, BlockchainAccountState.WAITING_VALIDATION);
-
-        legalEntityId_To_Addr[cnpj] = addr;
-
-        emit AccountRegistration(addr, cnpj, idProofHash);
+        emit AccountRegistration(addr, id, idProofHash);
     }
 
-
-
-   /**
-    * Changes the original link between CNPJ and Ethereum account.
-    * The new link still needs to be validated by BNDES.
-    * This method can only be called by BNDESToken contract because BNDESToken can pause and because there are
-    * additional instructions there.
-    * @param cnpj Brazilian identifier to legal entities
-    * @param newAddr the new address to be associated with the legal entity
-    * @param idProofHash The legal entities have to send BNDES a PDF where it assumes as responsible for an Ethereum account.
-    *                   This PDF is signed with eCNPJ and send to BNDES.
-    */
-    function changeAccountLegalEntity(uint cnpj, address newAddr, string memory idProofHash)
-    public {
-
-        address oldAddr = getBlockchainAccount(cnpj);
-
-        // Tem que haver um endereço associado a esse cnpj/subcrédito
-        require(!isResponsibleForRegistryValidation(oldAddr), "Não pode trocar endereço de conta de validação");
-
-        require(!isAvailableAccount(oldAddr), "Tem que haver um endereço associado a esse cnpj");
-
-        require(isAvailableAccount(newAddr), "Novo endereço não está disponível");
-
-        require (RBBLib.isValidHash(idProofHash), "O hash da declaração é inválido");
-
-        // Aponta o novo endereço para o novo LegalEntityInfo
-        legalEntitiesInfo[newAddr] = LegalEntityInfo(cnpj, idProofHash, BlockchainAccountState.WAITING_VALIDATION);
-
-        // Apaga o mapping do endereço antigo
-        legalEntitiesInfo[oldAddr].state = BlockchainAccountState.INVALIDATED_BY_CHANGE;
-
-        // Aponta mapping CNPJ para newAddr
-        legalEntityId_To_Addr[cnpj] = newAddr;
-
-        emit AccountChange(oldAddr, newAddr, cnpj, idProofHash);
-
+    modifier onlyUnpausedAccount() {
+        if ( legalEntitiesInfo[msg.sender].state != BlockchainAccountState.PAUSED )
+        _;
     }
 
    /**
-    * Validates the initial registry of a legal entity or the change of its registry
+    * Validates the initial registry of a legal entity 
     * @param addr Ethereum address that needs to be validated
     */
-    function validateRegistryLegalEntity(address addr) public {
+    function validateRegistryLegalEntity(address addr) public onlyUnpausedAccount {
 
-        require(isResponsibleForRegistryValidation(msg.sender), "Somente o responsável pela validação pode validar contas");
+        address responsible = msg.sender;
+
+        require(isResponsibleForRegistryValidation(responsible), "Somente responsável pela validação pode validar contas");
 
         require(legalEntitiesInfo[addr].state == BlockchainAccountState.WAITING_VALIDATION,
             "A conta precisa estar no estado Aguardando Validação");
 
         legalEntitiesInfo[addr].state = BlockchainAccountState.VALIDATED;
 
-        emit AccountValidation(addr, legalEntitiesInfo[addr].id);
+        emit AccountValidation(addr, legalEntitiesInfo[addr].id, responsible);
+    }
+
+/**
+    * Pause an account     
+    * @param addr Ethereum address that needs to be validated
+    */
+    function pauseRegistryLegalEntity(address addr) public onlyUnpausedAccount {
+
+        address responsible = msg.sender;
+
+        require( isValidatedAccount(responsible) , "Somente uma conta responsável válida pode pausar outras contas");
+        require( isValidatedAccount(addr) , "Somente uma conta válida pode ser pausada");
+
+        legalEntitiesInfo[addr].state = BlockchainAccountState.PAUSED;
+        
+        emit AccountPaused(addr, legalEntitiesInfo[addr].id, responsible);
+    }
+
+
+/**
+    * Unpause an account     
+    * @param addr Ethereum address that needs to be validated
+    */
+    function unpauseRegistryLegalEntity(address addr) public onlyUnpausedAccount {
+
+        address responsible = msg.sender;
+
+        require( isResponsibleForRegistryValidation(responsible) , "Somente uma conta responsável validadora pode despausar outras contas" );        
+        require( ( legalEntitiesInfo[addr].state == BlockchainAccountState.PAUSED ) , "Somente uma conta pausada pode ser despausada" ); 
+
+        legalEntitiesInfo[addr].state = BlockchainAccountState.VALIDATED;
+        
+        emit AccountUnpaused(addr, legalEntitiesInfo[addr].id, responsible);
     }
 
 
@@ -158,30 +140,33 @@ contract RBBRegistry is Ownable() {
     * The invalidation can be called at any time in the lifecycle of the address (not only when it is WAITING_VALIDATION)
     * @param addr Ethereum address that needs to be validated
     */
-    function invalidateRegistryLegalEntity(address addr) public {
+    function invalidateRegistryLegalEntity(address addr) public onlyUnpausedAccount {
 
-        require(isResponsibleForRegistryValidation(msg.sender), "Somente o responsável pela validação pode invalidar contas");
+        address responsible = msg.sender;
 
-        require(!isResponsibleForRegistryValidation(addr), "Não é possível invalidar conta do responsável pela validação de contas");
+        require(isResponsibleForRegistryValidation(responsible), "Somente responsável pela validação pode invalidar contas");
 
-        legalEntitiesInfo[addr].state = BlockchainAccountState.INVALIDATED_BY_VALIDATOR;
+        legalEntitiesInfo[addr].state = BlockchainAccountState.INVALIDATED;
         
-        emit AccountInvalidation(addr, legalEntitiesInfo[addr].id);
+        emit AccountInvalidation(addr, legalEntitiesInfo[addr].id, responsible);
     }
 
 
    /**
     * By default, the owner is also the Responsible for Validation.
     * The owner can assign other address to be the Responsible for Validation.
-    * @param rs Ethereum address to be assigned as Responsible for Validation.
+    * @param addr Ethereum address to be assigned as Responsible for Validation.
     */
-    function setResponsibleForRegistryValidation(address rs) public onlyOwner {
-        responsibleForRegistryValidation = rs;
-        //TODOO: evento quando trocar papeis
+    function setResponsibleForRegistryValidation(address addr) public {
+        require ( ( legalEntitiesInfo[addr].role == BlockchainAccountRole.ADMIN || 
+                 legalEntitiesInfo[addr].role == BlockchainAccountRole.SYSTEM || isOwner(addr) ), "Apenas Owener, Admin ou System podem dar poder de Admin");
+        legalEntitiesInfo[addr].role = BlockchainAccountRole.ADMIN;        
+        emit AccountAdminUpgrade(addr, legalEntitiesInfo[addr].id, msg.sender);
     }
 
     function isResponsibleForRegistryValidation(address addr) public view returns (bool) {
-        return (addr == responsibleForRegistryValidation);
+        return ( legalEntitiesInfo[addr].role == BlockchainAccountRole.ADMIN || 
+                 legalEntitiesInfo[addr].role == BlockchainAccountRole.SYSTEM   );
     }
 
     function isOwner(address addr) public view returns (bool) {
@@ -192,8 +177,6 @@ contract RBBRegistry is Ownable() {
         return legalEntitiesInfo[addr].state == BlockchainAccountState.AVAILABLE;
     }
 
-//TODO: eh necessario ter metodos que verificam se id ou address estao validos
-
     function isWaitingValidationAccount(address addr) public view returns (bool) {
         return legalEntitiesInfo[addr].state == BlockchainAccountState.WAITING_VALIDATION;
     }
@@ -202,58 +185,42 @@ contract RBBRegistry is Ownable() {
         return legalEntitiesInfo[addr].state == BlockchainAccountState.VALIDATED;
     }
 
-    function isValidatedId(uint id) public view returns (bool) {
-        address addr = getBlockchainAccount(id);
-        return isValidatedAccount(addr);
-    }
-
-    function isInvalidatedByValidatorAccount(address addr) public view returns (bool) {
-        return legalEntitiesInfo[addr].state == BlockchainAccountState.INVALIDATED_BY_VALIDATOR;
-    }
-
-    function isInvalidatedByChangeAccount(address addr) public view returns (bool) {
-        return legalEntitiesInfo[addr].state == BlockchainAccountState.INVALIDATED_BY_CHANGE;
-    }
-
-    function getResponsibleForRegistryValidation() public view returns (address) {
-        return responsibleForRegistryValidation;
+    function isInvalidated(address addr) public view returns (bool) {
+        return legalEntitiesInfo[addr].state == BlockchainAccountState.INVALIDATED;
     }
 
     function getId (address addr) public view returns (uint) {
         return legalEntitiesInfo[addr].id;
     }
 
-    function getLegalEntityInfo (address addr) public view returns (uint, string memory, uint, address) {
-        return (legalEntitiesInfo[addr].id, legalEntitiesInfo[addr].idProofHash, (uint) (legalEntitiesInfo[addr].state),
-             addr);
+    function getLegalEntityInfo (address addr) public view returns (uint, string memory, uint, uint, address) {
+        return (  legalEntitiesInfo[addr].id, 
+                  legalEntitiesInfo[addr].idProofHash, 
+                  (uint) (legalEntitiesInfo[addr].state),
+                  (uint) (legalEntitiesInfo[addr].role),
+                  addr 
+               );
     }
 
-    function getBlockchainAccount(uint cnpj) public view returns (address) {
-        return legalEntityId_To_Addr[cnpj];
-    }
-
-    function getLegalEntityInfoById (uint cnpj) public view
-        returns (uint, string memory, uint, address) {
-        
-        address addr = getBlockchainAccount(cnpj);
-        return getLegalEntityInfo (addr);
+    function getBlockchainAccounts(uint id) public view returns (address[] memory) {
+        return legalEntityId_To_Addr[id];
     }
 
     function getAccountState(address addr) public view returns (int) {
         return ((int) (legalEntitiesInfo[addr].state));
     }
 
-    function registryMock(uint cnpj)
+    function registryMock(uint id)
         public {
         
         address addr = msg.sender;
         string memory idProofHash = "";
 
-        legalEntitiesInfo[addr] = LegalEntityInfo(cnpj, idProofHash, BlockchainAccountState.VALIDATED);
+        legalEntitiesInfo[addr] = LegalEntityInfo(id, idProofHash, BlockchainAccountState.VALIDATED, BlockchainAccountRole.ADMIN );
 
-        legalEntityId_To_Addr[cnpj] = addr;
+        legalEntityId_To_Addr[id].push(addr);
 
-        emit AccountRegistration(addr, cnpj, idProofHash);
+        emit AccountRegistration(addr, id, idProofHash);
     }
 
 
