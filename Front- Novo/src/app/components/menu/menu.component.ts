dff385c8dae9078b9f7f908fde75709569343faf
José Nogueira <josej@bdnes.gov.br>
2020-11-18 17:49:18 -0300

    identificacao no topo

--- Front- Novo/src/app/components/menu/menu.component.ts
+++ Front- Novo/src/app/components/menu/menu.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 
+import { Web3Service } from '../../Web3Service';
 
 
 @Component({
@@ -11,15 +12,36 @@ export class MenuComponent implements OnInit {
  
   @Output() toggleSideBarForMe: EventEmitter<any> = new EventEmitter();
 
-  constructor() { }
+  usuario : any;
+  selectedAccount : any;
 
-  ngOnInit():void { }
+  constructor(private web3Service: Web3Service) { 
+    
+  }
+
+  ngOnInit():void { 
+
+    setInterval(async () => {
+      this.selectedAccount = await this.web3Service.getCurrentAccountSync();
+      console.log(this.selectedAccount);
+      this.usuario = await this.recuperaRegistroBlockchain(this.selectedAccount);
+  }, 3000)
+  }
 
   toggleSideBar(){
     this.toggleSideBarForMe.emit();
   }
-
   
+  async recuperaRegistroBlockchain(enderecoBlockchain) : Promise<any> {
+    if (enderecoBlockchain != undefined && enderecoBlockchain != null) {
+        let usuario = await this.web3Service.getPJInfoSync(enderecoBlockchain);
+        return usuario;
+    } else {
+        console.log('this.usuario');
+        console.log(this.usuario);
+    }
+    
+}  
 
   
 }
