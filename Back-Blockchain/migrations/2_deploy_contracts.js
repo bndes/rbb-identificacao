78eb658e1a6942eec94d300c8ce3589ce662bc93
José Nogueira <josej@bndes.gov.br>
2021-03-12 17:01:23 -0300

    adicionaRBBRegistry v2 no deployment

--- Back-Blockchain/migrations/2_deploy_contracts.js
+++ Back-Blockchain/migrations/2_deploy_contracts.js
@@ -1,6 +1,8 @@
 var RBBRegistry = artifacts.require("./RBBRegistry.sol");
 var RBBLib = artifacts.require("./RBBLib.sol");
 
+var RBBRegistry_v2 = artifacts.require("./RBBRegistry_v2.sol");
+
 module.exports = async (deployer) => {
 	await deployer.deploy(RBBLib);
 	await deployer.link(RBBLib, RBBRegistry);
@@ -9,5 +11,6 @@ module.exports = async (deployer) => {
 	let hash = "9c46ae9957f4589d4a4c50ff4eaf01a2516f755e20102ab59403c484e086f647"; //TODO: change the 9c46ae9957f4589d4a4c50ff4eaf01a2516f755e20102ab59403c484e086f647 to the hash of BNDES auto-declaration of address property and liability
 	let daysToExpire = 3650;
 	await deployer.deploy(RBBRegistry, CNPJ, hash , daysToExpire, {gas: 6721975})
+	await deployer.deploy(RBBRegistry_v2, {gas:  804247552})
 	
 };
