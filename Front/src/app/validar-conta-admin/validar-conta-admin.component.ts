e1e6933683804273798ae92b83aa685377c088bd
Leonardo <leonardopereiradossantos@id.uff.br>
2021-03-16 16:46:32 -0300

    resolvendo problema do Dashboard GC

--- Front/src/app/validar-conta-admin/validar-conta-admin.component.ts
+++ Front/src/app/validar-conta-admin/validar-conta-admin.component.ts
@@ -129,7 +129,7 @@ export class ValidarContaAdminComponent implements OnInit {
         this.listaTransacoesPJ = [];   
       }
       this.monitoraEventos();  
-    }, 1500)
+    }, 60000)
 
     setInterval(() => {
       this.estadoLista = this.estadoLista === "undefined" ? "vazia" : "cheia"          
@@ -141,7 +141,7 @@ export class ValidarContaAdminComponent implements OnInit {
         this.dataSource.sort = this.sort;
         this.ref.detectChanges()
       }
-    }, 6300)
+    }, 63)
   
     
     //const users = Array.from({length: 1}, (_, k) => createNewUser(k + 1));
